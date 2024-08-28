bd62261c5e86d8839f25f7b91965bb20436b1cb9
Kaspar Moberg <kaspeti123@gmail.com>
2024-08-29 00:51:27 +0200

    fixed autotag

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -7,5 +7,21 @@ return {
         -- this is equalent to setup({}) function
     },
 
-    { "windwp/nvim-ts-autotag" },
+    { 
+        "windwp/nvim-ts-autotag",
+        config = function() 
+            require('nvim-ts-autotag').setup({
+                opts = {
+                    -- Defaults
+                    enable_close = true, -- Auto close tags
+                    enable_rename = true, -- Auto rename pairs of tags
+                    enable_close_on_slash = false -- Auto close on trailing </
+                },
+                -- Also override individual filetype configs, these take priority.
+                -- Empty by default, useful if one of the "opts" global settings
+                -- doesn't work well in a specific filetype
+                per_filetype = {}
+            })
+        end
+    },
 }
