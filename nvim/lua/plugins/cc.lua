0d2d07c3763eb4e49577371ae8c27d6c37e11eab
kaspar.moberg <kaspar.moberg@student.uib.no>
2024-09-02 08:31:24 +0200

    feat: added conventional commits

---
+++ nvim/lua/plugins/cc.lua
@@ -0,0 +1,27 @@
+return {
+    { 
+        "olacin/telescope-cc.nvim",
+        config = function() 
+            local function create_conventional_commit()
+                local actions = require("telescope._extensions.conventional_commits.actions")
+                local picker = require("telescope._extensions.conventional_commits.picker")
+                local themes = require("telescope.themes")
+
+                -- if you use the picker directly you have to provide your theme manually
+                local opts = {
+                    action = actions.prompt,
+                    include_body_and_footer = true,
+                }
+                opts = vim.tbl_extend("force", opts, themes["get_ivy"]())
+                picker(opts)
+            end
+
+            vim.keymap.set(
+            "n",
+            "cc",
+            create_conventional_commit,
+            { desc = "Create conventional commit" }
+            )
+        end
+    }
+}
