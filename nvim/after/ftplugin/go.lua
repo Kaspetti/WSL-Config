9516fc5e525e1c7a107dfa9ad1a857ae7140cb19
Kaspar Moberg <kaspeti123@gmail.com>
2024-12-09 07:28:51 +0100

    feat: added inlay hints plugin and config for gopls

---
+++ nvim/after/ftplugin/go.lua
@@ -0,0 +1,15 @@
+require("lspconfig").gopls.setup({
+  settings = {
+    gopls = {
+      hints = {
+        rangeVariableTypes = true,
+        parameterNames = true,
+        constantValues = true,
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        functionTypeParameters = true,
+      },
+    }
+  }
+})
---
+++ nvim/lua/plugins/inlay.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "MysticalDevil/inlay-hints.nvim",
+        event = "LspAttach",
+        dependencies = { "neovim/nvim-lspconfig" },
+        config = function()
+            require("inlay-hints").setup()
+        end
+    }
+}
