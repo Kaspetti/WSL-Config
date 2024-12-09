585c8c32b7e984b86ab8937af2fcc937575f11dd
Kaspar Moberg <kaspeti123@gmail.com>
2024-12-09 07:30:00 +0100

    fix: fixed some keybindings for hop plugin

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -6,13 +6,13 @@ return {
             require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
             vim.api.nvim_set_keymap('n', 's', "<cmd>HopChar1<cr>", {})
             vim.api.nvim_set_keymap('n', 'S', "<cmd>HopChar2<cr>", {})
-            vim.api.nvim_set_keymap('n', 'w', "<cmd>HopWord<cr>", {})
+            vim.api.nvim_set_keymap('n', 'W', "<cmd>HopWord<cr>", {})
             vim.api.nvim_set_keymap('n', 'f', "<cmd>HopPattern<cr>", {})
-            vim.api.nvim_set_keymap('n', 'l', "<cmd>HopLine<cr>", {})
+            vim.api.nvim_set_keymap('n', 'L', "<cmd>HopLine<cr>", {})
             vim.api.nvim_set_keymap('v', 's', "<cmd>HopChar1<cr>", {})
             vim.api.nvim_set_keymap('v', 'S', "<cmd>HopChar2<cr>", {})
-            vim.api.nvim_set_keymap('v', 'w', "<cmd>HopWord<cr>", {})
-            vim.api.nvim_set_keymap('v', 'l', "<cmd>HopLine<cr>", {})
+            vim.api.nvim_set_keymap('v', 'W', "<cmd>HopWord<cr>", {})
+            vim.api.nvim_set_keymap('v', 'L', "<cmd>HopLine<cr>", {})
         end
     }
 }
